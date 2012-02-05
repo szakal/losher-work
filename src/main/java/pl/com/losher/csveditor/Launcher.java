37e0afee466abf5ad668f7195754376548318670
Tomek <tomek.kramarczyk@gmail.com>
2012-02-05 11:54:41 +0100

    dodanie logbacka

--- src/main/java/pl/com/losher/csveditor/Launcher.java
+++ src/main/java/pl/com/losher/csveditor/Launcher.java
@@ -1,14 +1,22 @@
 package pl.com.losher.csveditor;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class Launcher {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     public void launch() {
+
+        log.info("Starting Spring Application Context.");
+
         //konfiguracja kontekstu springa. Aplikacja uruchamia sie (pokazuje) poprzez wywolanie metody init - w springu.
         String[] contextPaths = new String[]{"pl/com/losher/csveditor/applicationContext*"};
         new ClassPathXmlApplicationContext(contextPaths);
+
+        log.info("Application has started.");
     }
 
 }
--- src/main/java/pl/com/losher/csveditor/view/listeners/ExitListener.java
+++ src/main/java/pl/com/losher/csveditor/view/listeners/ExitListener.java
@@ -1,22 +1,31 @@
 package pl.com.losher.csveditor.view.listeners;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 /**
+ * //TODO fajnie jakby ten event odpalal sie tez po kliknieciu w "x" w prawym gornym.
  *
  * @author tomasz.kramarczyk
  */
-public class ExitListener extends AbstractAction{
+public class ExitListener extends AbstractAction {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     /**
      * Wylacza aplikacje
+     *
      * @param e event
      */
     @Override
     public void actionPerformed(ActionEvent e) {
         //TODO moze jakies okientko "Jestes pewny"??
+        log.info("Application is getting to shutdown...");
         System.exit(0);
+
     }
 
 }
